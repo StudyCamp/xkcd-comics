81bbde6db3def55e3ac01bd7049b29ad9c46fe4b
unknown <com>
2021-06-10 03:12:45 -0400

    added css features

---
+++ public/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+    lightSwitch();
+    backgroundSwitch();
+});
+
+function lightSwitch() {
+    const lightSwitch = document.querySelector("#light-switch");
+    const lightMover = document.querySelector("#light-mover");
+    lightSwitch.onclick = function() { 
+        if  (lightSwitch.textContent == "flashlight_on") {
+            lightSwitch.textContent = "flashlight_off";
+            lightMover.style.display = 'none';
+        } else {
+            lightSwitch.textContent = "flashlight_on";
+            lightMover.style.display = 'block';
+        } 
+    }       
+};
+
+
+function backgroundSwitch() {
+    const hasImage = "linear-gradient(130deg, rgba(0, 21, 206, 0.623),rgba(199, 0, 43, 0.514)), url('../img/girl4-crop1.jpg')";
+    const noImage =  "url('[INSERT HTTPS IMAGE URL HERE]'), linear-gradient(130deg, rgba(0, 21, 206, 0.623),rgba(199, 0, 43, 0.514))";
+    const backgroundSwitch = document.querySelector("#background-switch");
+    backgroundSwitch.onclick = function() { 
+        if  (backgroundSwitch.textContent == "image") {
+            backgroundSwitch.textContent = "hide_image"; 
+            document.body.style.backgroundImage = noImage;
+        } else {
+            backgroundSwitch.textContent = "image";
+            document.body.style.backgroundImage = hasImage;
+
+        } 
+    }       
+};
+
